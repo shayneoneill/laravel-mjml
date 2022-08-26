c56be7411b369a122f3c722ff21f382d18955591
shayneoneill <shayneoneill@users.noreply.github.com>
2022-08-26 13:11:01 +0800

    Fix HTML2TEXT namespace
    Adds fix from: https://github.com/asahasrabuddhe/laravel-mjml/pull/133

--- src/Process/MJML.php
+++ src/Process/MJML.php
@@ -2,7 +2,8 @@
 
 namespace Asahasrabuddhe\LaravelMJML\Process;
 
-use Html2Text\Html2Text;
+// Reference: https://github.com/asahasrabuddhe/laravel-mjml/pull/133
+use Soundasleep\Html2Text;
 use Illuminate\Support\Facades\File;
 use Illuminate\Support\HtmlString;
 use Illuminate\View\View;
